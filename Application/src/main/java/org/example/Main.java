11cd34bdc4d1261bd61a8b4785c42bc6669d4d41
playfulCloud <godlygener@gmail.com>
2024-02-29 14:45:34 +0100

    initial

---
+++ Application/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//TODO:
+//TODO: zipowac wybrane pliki i katologi
+//TODO: obliczyc funkcje md5 dla zipowanego katalogu
+//TODO: napisac validator sprawdzający czy md5 sie zgadza dla wybranego pliku
+//TODO: bibblioteka z zipowanie
+//TODO: biblioteka z md5
+//TODO: i drugi projekt z guicem
+//TODO: jlink i środowisko uruchomieniowe
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
